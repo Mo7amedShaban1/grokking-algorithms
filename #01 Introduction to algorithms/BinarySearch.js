ef8db2c3415d60187bf307bff34a1749d46344d5
Mohamed Shaaban <mohamedshaaban@Mohameds-MacBook-Pro.local>
2021-10-10 12:39:21 +0200

    add recursive solution

--- #01 Introduction to algorithms/BinarySearch.js
+++ #01 Introduction to algorithms/BinarySearch.js
@@ -1,4 +1,7 @@
-const binarySearch = (list, item) => {
+const list = Array.from(Array(1000), (value, index) => index);
+
+// Method 1:
+const binarySearch1 = (list, target) => {
   var low = 0;
   var high = list.length - 1;
 
@@ -6,18 +9,31 @@ const binarySearch = (list, item) => {
     const mid = parseInt(low + (high - low) / 2);
     const guess = list[mid];
 
-    if (item === guess) {
+    if (target === guess) {
       return mid;
-    } else if (item > guess) {
+    } else if (target > guess) {
       low = mid + 1;
-    } else if (item < guess) {
+    } else if (target < guess) {
       high = mid - 1;
     }
   }
 
-  return null;
+  return -1;
 };
 
-const list = Array.from(Array(1000000), (value, index) => index);
+// Method 2:
+const binarySearch2 = (list, target, start, end) => {
+  if (start > end) return -1;
 
-binarySearch(list, 99999);
+  var middle = parseInt(start + (end - start) / 2);
+
+  if (list[middle] === target) return middle;
+
+  if (list[middle] > target)
+    return binarySearch2(list, target, start, middle - 1);
+  if (list[middle] < target)
+    return binarySearch2(list, target, middle + 1, end);
+};
+
+console.log(binarySearch1(list, 400));
+console.log(binarySearch2(list, 498, 0, list.length - 1));
